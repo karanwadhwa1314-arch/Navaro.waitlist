de475125467ffb2e8b00fc0bb42c7985547a678a
Ankit A K <ankitak.flare@gmail.com>
2026-08-15 17:33:49 +0530

    logo reizing

--- components/waitlist/WaitlistLanding.tsx
+++ components/waitlist/WaitlistLanding.tsx
@@ -20,7 +20,7 @@ export default function WaitlistLanding() {
           <img
             src="/image/Nav_bar-.png"
             alt="Navaro"
-            className="h-[52px] w-auto object-contain max-[500px]:h-[40px]"
+            className="h-[91px] w-auto object-contain max-[500px]:h-[70px]"
           />
         </Link>
       </header>
